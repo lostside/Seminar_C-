d8eef80a653177cd2e19fb09fde15021a1637c5c
Denis <lost.ned@gmail.com>
2022-11-21 21:22:28 +0300

    Delete trash

--- Sem001/task0/Program.cs
+++ Sem001/task0/Program.cs
@@ -9,8 +9,8 @@
 
 
 Console.WriteLine("Введите число");
-//int number = Convert.ToInt32(Console.ReadLine()); - конвертация строки в интеджер, 
-// так как команда ReadLine получает строку на выходе
+//int number = Convert.ToInt32(Console.ReadLine()); - конвертация string в int, 
+// так как команда ReadLine получает строку (string) на выходе.
 
 string answer = Console.ReadLine();
 int number = Convert.ToInt32(answer);
