937e2f1b02973308b1f4b14490e63606fde5c5e4
Denis <lost.ned@gmail.com>
2022-11-24 20:08:58 +0300

    закомментил задачу

--- Sem001/task3/Program.cs
+++ Sem001/task3/Program.cs
@@ -3,14 +3,14 @@
 // 5 -> Пятница
 
 
-Console.WriteLine("Введите число от 1 до 7");
-string? answer = Console.ReadLine();
-int number = Convert.ToInt32(answer);
+// Console.WriteLine("Введите число от 1 до 7");
+// string? answer = Console.ReadLine();
+// int number = Convert.ToInt32(answer);
 
 
-//if (number || 7 )  
+// //if (number || 7 )  
 
-    Console.WriteLine("Понедельник");
+//     Console.WriteLine("Понедельник");
 
-else
-{}
+// else
+// {}
