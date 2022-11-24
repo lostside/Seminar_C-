7112528b78ff74f756b0ecf5cde9b967b5cd56d5
Denis <lost.ned@gmail.com>
2022-11-24 23:03:48 +0300

    Добавлена task10

--- Sem002/task10/Program.cs
+++ Sem002/task10/Program.cs
@@ -5,3 +5,13 @@
 // 782 -> 8
 // 918 -> 1
 
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int digit2 = number % 10; 
+int digit1 = number / 10;
+int digit3 = digit1 % 10;
+
+Console.WriteLine(digit3);
+
+Console.WriteLine($"Искомое число = {digit3}");
