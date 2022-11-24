cad5bfd95acfe2280e1cbc708f2663b953a13cad
Denis <lost.ned@gmail.com>
2022-11-24 21:10:18 +0300

    Добавлена task11,12

---
+++ Sem002/task11/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000); 
+
+Console.WriteLine(number);
+
+int digit2 = number % 10; 
+int digit1 = number / 100;
+
+Console.WriteLine(digit1);
+Console.WriteLine(digit2);
+
+Console.WriteLine($"Искомое число = {digit1 * 10 + digit2}");
---
+++ Sem002/task12/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, 
+//является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+
+// 34, 5 -> не кратно, остаток 4 
+// 16, 4 -> кратно
+
+Console.WriteLine("Введите первое число");
+int digit1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int digit2 = Convert.ToInt32(Console.ReadLine());
+
+if (digit1 % digit2 == 0)
+{
+    Console.WriteLine("Они кратны");
+
+}
+else
+{
+    Console.WriteLine($"Остаток от деления = {digit1 % digit2}");
+}
