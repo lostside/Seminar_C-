e488a693d8c3d4fa12f6ddadeed22460ff08695e
Denis <lost.ned@gmail.com>
2022-11-24 21:37:53 +0300

    Добавлены task14,16

---
+++ Sem002/task14/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23
+//
+// 14 -> нет
+// 45 -> нет
+// 161 -> да
+
+Console.WriteLine("Введите число");
+int digit = Convert.ToInt32(Console.ReadLine());
+
+if (digit % 7 == 0 && digit % 23 == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine("Не кратны");
+}
---
+++ Sem002/task16/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход 2 числа и проверяет, 
+// является ли одно число квадратом другого
+
+// 5 и 25 -> да
+// -4 и 16 -> да
+// 8 и 9 -> нет
+
+Console.WriteLine("Введите первое число");
+int digit1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int digit2 = Convert.ToInt32(Console.ReadLine());
+
+if (digit2 == digit1 * digit1)
+{
+    Console.WriteLine($"{digit2} является квадратом {digit1}");
+}
+else
+{
+    Console.WriteLine($"{digit2} не является квадратом {digit1}");
+}
