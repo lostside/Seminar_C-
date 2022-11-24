843251a7f6064d5e03107eb9808282061781c8a7
Denis <lost.ned@gmail.com>
2022-11-24 23:32:07 +0300

    Добавлена task15

--- Sem002/task15/Program.cs
+++ Sem002/task15/Program.cs
@@ -4,3 +4,31 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.WriteLine("Введите порядковый номер дня недели");
+
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day == 6 || day == 7)
+{
+    Console.WriteLine("Это выходной, отдыхай!");
+}
+else
+
+    if (day < 6 )
+    {
+        Console.WriteLine("Жди выходных, они близко");
+    }
+
+// else
+
+//     if (day == 3){
+//             Console.WriteLine("Среда - маленькая пятница)");
+//         }
+   
+// else
+//    if (day == 5)
+//     {
+//         Console.WriteLine("Пяятниицаа");
+//     }
+
