02da6bc37f040e73e550101faf8951cb70ad2e81
Denis <lost.ned@gmail.com>
2022-11-28 21:43:04 +0300

    добавлена доп задача

---
+++ Sem003/task22/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 22: Напишите программу, которая принимает на вход число 
+// (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i < N; i++)
+{
+    double sqr = Math.Pow(i, 2);
+    Console.Write(sqr + ", ");
+}
+Console.Write(Math.Pow(N, 2));
+
+// for (int i = 2; i < number; i = i + 2)
+// if (i % 2 == 0)
+// {
+//  Console.Write(i + " ");
+// }
---
+++ Sem003/taskDop/Program.cs
@@ -0,0 +1,22 @@
+﻿// int []A = {1, 3, 4, 5, 7, 8};
+
+// for (int i =0; i < (A.Leight /2); i++);
+// {
+//     temp = A[i];
+//     A[i] = A.Length - 1;
+//     A.Length - 1 = temp;
+// }
+int[] array = {2,4,9,5,7,34,12,5,1};
+        int size = array.Length;
+        int index = 0;
+        while (index<size/2)
+        {
+            int temp = array[size-1-index];
+            array[size-1-index] = array[index];
+            array[index]=temp;
+            index++;
+        }
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i]+" ");
+}
