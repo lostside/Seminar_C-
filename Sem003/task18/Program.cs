eb2e4137f2d877c5608e1da97f6f17f994005448
Denis <lost.ned@gmail.com>
2022-11-28 21:06:36 +0300

    Добавлены задача 18, 20

---
+++ Sem003/task18/Program.cs
@@ -0,0 +1,33 @@
+﻿
+// Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+// 1: x > 0 && y > 0
+// 2: x < 0 && y > 0
+// 3: x < 0 && y < 0
+// 4: x > 0 && y < 0
+
+Console.WriteLine("Введите  номер координатной четверти");
+int quarte = Convert.ToInt32(Console.ReadLine());
+
+
+if (quarte == 1)
+{
+    Console.WriteLine("x > 0 и y > 0");
+}
+else if (quarte == 2)
+{
+    Console.WriteLine(" x < 0 и y > 0");
+}
+else if (quarte == 3)
+{
+    Console.WriteLine(" x < 0 и y < 0");
+}
+else if (quarte == 4)
+{
+    Console.WriteLine(" x > 0 и y < 0");
+}
+else
+{
+    Console.WriteLine(" Вы ввели не правильный номер четверти");
+}
---
+++ Sem003/task20/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 20: Напишите программу, которая принимает на вход координаты двух точек и находит 
+//расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+Console.WriteLine("Введите координату x точки A");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату y точки A");
+int y = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Введите координату x2 точки B");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату y2 точки A");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double z = Math.Sqrt((x2 - x) * (x2 -x ) + (y2 - y) * (y2 - y));
+
+Console.WriteLine($"{z:f2}"); // два знака после запятой
+
