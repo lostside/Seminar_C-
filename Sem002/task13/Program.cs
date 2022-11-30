68791403a7b45e274dbe6392a2d000363e5eb597
Denis <lost.ned@gmail.com>
2022-11-30 23:39:56 +0300

    Жалкая попытка написания task13

--- Sem002/task13/Program.cs
+++ Sem002/task13/Program.cs
@@ -11,6 +11,34 @@
 
 
 Console.WriteLine("Введите число");
-
 int n = Convert.ToInt32(Console.ReadLine());
+int rez = 1;
+// if (n <= 9999)
+// {
+//     rez = n / 10;
+//     rez = rez % 10;
+//     Console.WriteLine(rez);
+// }
+// else if (n <= 99999)
+// {
+//     int rez1 = n / 100;
+//     rez1 = rez1 % 10;
+//     Console.WriteLine(rez1);
+// }
+// if (n <= 99 && n > 100)
+// {
+//     Console.WriteLine("rez");
+// }
+// while (n >= 9999)
+// {
+//     int rez1 = n / 100;
+//     rez = rez1 % 10;
+//     Console.WriteLine(rez);
+//     break;
+//     //if (n < 999)
+
+    
+// }
+//Console.WriteLine("no");
+
 
