915f554bcaf65e0cc978b4e7ce6f60e95e5fb7e7
Denis <lost.ned@gmail.com>
2022-12-01 20:30:49 +0300

    Добавлен task 24

---
+++ Sem004/task24/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход 
+// число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+
+bool Validate(int number)
+{
+    if (number > 1)
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+int sum1toA(int A)
+{
+    int sum = 0; // объявляем вне цикла, так как в цикле будет обнуляться
+    for (int i = 1; i <= A; i++)
+    {
+        //sum = sum + i;
+        sum += i;
+    }
+    return sum;
+}
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = GetNumber("Введите число");
+bool IsCorrect = Validate(number);
+if (IsCorrect == true)
+{
+    int sum = sum1toA(number);
+    Console.WriteLine($"Сумма чисел от 1 до {number} = {sum}");
+}
+else
+{
+    Console.WriteLine($"Невозможно получить сумму от 1 до {number}");
+}
