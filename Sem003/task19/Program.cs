a2cac9bdb1b755c27293b2a28947047ff0c6ec09
Denis <lost.ned@gmail.com>
2022-12-06 16:55:00 +0300

    Добавлена task19

--- Sem003/task19/Program.cs
+++ Sem003/task19/Program.cs
@@ -5,14 +5,20 @@
 // 12821 -> да
 // 23432 -> да
 
-// Console.WriteLine("Введите пятизначное число");
-// int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите пятизначное число");
+int n = Convert.ToInt32(Console.ReadLine());
 
-//  if (n > 99999 && n < 10000)
-//  {
-//      Console.WriteLine("Введи еще раз 5 цифр");
-// }
-// else if ( )
-//  {
-    
-//  }
+
+int part1 = n / 10000; // 1
+int part2 = (n / 1000) % 10; // 2е число
+int part3 = n % 10; // 5 число
+int part4 = ( n % 100) / 10; // 4 число
+
+if( part1 == part3 && part2 == part4)
+{
+    Console.WriteLine("Это палиндром!");
+}
+else
+{
+    Console.WriteLine("Не палиндром");
+}
