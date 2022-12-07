17a2f329bbef96db6f4f01a2e0ce3b6a5a8cc088
Denis <lost.ned@gmail.com>
2022-12-07 18:43:48 +0300

    Добавлен task26

--- Sem004/task27/Program.cs
+++ Sem004/task27/Program.cs
@@ -6,3 +6,17 @@
 // 82 -> 10
 
 // 9012 -> 12
+
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+    int sum = 0;
+    while (number > 0)
+    {
+        sum = sum + number % 10;
+        number = number / 10;
+    }
+
+Console.WriteLine(sum);
