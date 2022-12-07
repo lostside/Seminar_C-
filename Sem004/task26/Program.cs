ecdc4587b9da8fb8257e09a3219cd961e583c99f
Denis <lost.ned@gmail.com>
2022-12-07 13:51:56 +0300

    Добавлен task26

--- Sem004/task26/Program.cs
+++ Sem004/task26/Program.cs
@@ -4,33 +4,6 @@
 // 78 -> 2
 // 89126 -> 5
 
-//int GetNumber(string message) // Метод для ввода числа
-//{
-//    Console.WriteLine("Введите число");
-//    return Convert.ToInt32(Console.ReadLine());
-//}
-
-
-//int Res(int num)
-//{
-//   int res = 0;
-//    int i = 0;
-//   for (i = 1; i > 0 ; i++)
-//    {
-//        res = res / 10;
-//    }
-    // while(num>0) 
-    // {
-    // num /= 10;
-    // i++;
-    // }
-//    return res;
-//}
-
-//int number = GetNumber("Введите число");
-//int count = Res(number);
-//Console.WriteLine(count);
-
 Console.WriteLine("Введите число");
 int number = Convert.ToInt32(Console.ReadLine());
 
