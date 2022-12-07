e95e52e826f49c621ea02f441a2fc7091a543b04
Denis <lost.ned@gmail.com>
2022-12-07 21:21:10 +0300

    Добавлен bool

---
+++ Sem005/bool/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] a = {1,2,3,4,5};
+int[] b = {1,2,3,4,5};
+
+bool res = true; // res = true - массивы равны, если res = false, то массивы не равны
+if (a.Length == b.Length)
+{
+    for (int i = 0; i < a.Length; i++)
+    {
+        if (a[i] != b[i])
+        {
+            res = false;
+        }
+    }
+    Console.WriteLine(res);
+}
