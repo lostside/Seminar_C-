7b87296dc7e561611d2929a8c2d180af807d948b
Denis <lost.ned@gmail.com>
2022-12-07 18:20:45 +0300

    Добавлен task30

--- Sem004/task29/Program.cs
+++ Sem004/task29/Program.cs
@@ -1,3 +1,16 @@
 ﻿// Задача 29: Напишите программу, которая задаёт массив из 8 
 // элементов и выводит их на экран. 
 // Данные вводятся с консоли пользователем
+
+
+int[] collection = new int[8];
+
+
+for (int i = 0; i < collection.Length; i ++)
+{
+    Console.Write("Введите элемент массива: ");
+    collection[i] = Convert.ToInt32(Console.ReadLine());
+}
+    
+
+Console.WriteLine(string.Join(", ", collection));
