e5edb80d6a339e90134477ee61b5c8b523898a19
Denis <lost.ned@gmail.com>
2022-12-07 17:12:43 +0300

    Добавлен task25

--- Sem004/task25/Program.cs
+++ Sem004/task25/Program.cs
@@ -5,3 +5,21 @@
 // 3, 5 -> 243 (3⁵)
 
 // 2, 4 -> 16
+
+
+int GetNumber(string message) // Метод для ввода числа
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int A = GetNumber("Введите число A");
+int B = GetNumber("Введите степень");
+int res = 1;
+    for (int i = 1; i <= B; i++)
+    {
+        res = res * A;
+        
+    }
+
+Console.WriteLine(res);
