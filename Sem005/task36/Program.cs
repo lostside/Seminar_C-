6373031ffd9223f87ec567eccf3f45300618aaf9
Denis <lost.ned@gmail.com>
2022-12-11 20:07:18 +0300

    add task38

---
+++ Sem005/task36/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+int[] GetRandomArray(int size, int leftRange, int rightRange)
+{
+    int[] array = new int[size];
+
+    Random rand = new Random();
+
+    for ( int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(leftRange, rightRange + 1); // rightRange + 1 = включая последний элемент
+    }
+    
+    return array;
+
+}
+
+const int SIZE = 4; // размер массива
+const int LEFTRANGE = -9;
+const int RIGHTRANGE = 99;
+
+int[] arr = GetRandomArray(SIZE, LEFTRANGE, RIGHTRANGE);
+Console.WriteLine($"[{string.Join(", ", arr)}]");
+
+int res = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    for(int j = 1; arr[j] % 2 != 0; j++) //if (arr[i] % 2 != 0)
+    {
+        res = arr[j] + arr[j + 2];
+    }
+}
+
+Console.WriteLine(res);
---
+++ Sem005/task38/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 38: Задайте массив натуральных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+int[] GetRandomArray(int size, int leftRange, int rightRange)
+{
+    int[] array = new int[size];
+
+    Random rand = new Random();
+
+    for ( int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(leftRange, rightRange + 1); // rightRange + 1 = включая последний элемент
+    }
+    
+    return array;
+
+}
+
+const int SIZE = 5; // размер массива
+const int LEFTRANGE = 0;
+const int RIGHTRANGE = 12;
+
+int[] arr = GetRandomArray(SIZE, LEFTRANGE, RIGHTRANGE);
+Console.WriteLine($"[{string.Join(", ", arr)}]");
+int i = 0;
+int maxArr = arr[0];
+int minArr = arr[i];
+for(i = 1; i < arr.Length; i++)
+{
+    if (arr[i] > maxArr)
+    {
+        maxArr = arr[i];
+    }
+      
+}
+for(i = 1; i < arr.Length;i++)
+{
+    if(arr[i] < minArr )
+    {
+        minArr = arr[i];
+    }
+}
+int diffMaxMin = maxArr - minArr;
+Console.WriteLine($"Максимальное значение массива {maxArr} - Минимальное значение массива {minArr}. Разница между значениями равна {diffMaxMin}");
