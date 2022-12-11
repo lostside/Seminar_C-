0e1bb2299293bb11ef670a0c0169e9e2ff4df7e4
Denis <lost.ned@gmail.com>
2022-12-11 23:42:47 +0300

    added 2 metod task42

--- Sem006/task42/Program.cs
+++ Sem006/task42/Program.cs
@@ -1,32 +1,44 @@
-﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
-// 45 -> 101101
-// 3 -> 11
-// 2 -> 10
+﻿// // Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// // 45 -> 101101
+// // 3 -> 11
+// // 2 -> 10
 
-Console.WriteLine("Введите число в десятичной системе исчисления");
-int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число в десятичной системе исчисления");
+// int n = Convert.ToInt32(Console.ReadLine());
 
-int Method1(int number)
+// int Method1(int number)
+// {
+//     int i = 0;
+//     while( number > 0)
+//     {
+//         number = number / 2;
+//         i++;
+//     }
+//     return i;
+
+// }
+
+// int[] collection = new int[Method1(n)];
+
+// int b = collection.Length-1;
+
+// while (n > 0)
+//     {
+//     collection[b] = n % 2;
+//     n = n / 2;
+//     b--;
+//     }
+
+// Console.WriteLine(string.Join("", collection));
+
+
+int a = 13;
+
+void PrintBinaryView(int number)
 {
-    int i = 0;
-    while( number > 0)
-    {
-        number = number / 2;
-        i++;
-    }
-    return i;
-
+    if ( number <= 0) return;
+    PrintBinaryView(number /2 );
+    Console.Write(number % 2);
 }
 
-int[] collection = new int[Method1(n)];
-
-int b = collection.Length-1;
-
-while (n > 0)
-    {
-    collection[b] = n % 2;
-    n = n / 2;
-    b--;
-    }
-
-Console.WriteLine(string.Join("", collection));
+PrintBinaryView(a);
