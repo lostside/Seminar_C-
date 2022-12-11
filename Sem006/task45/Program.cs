c19482cc5f62754b55175392f985a525cb3d9898
Denis <lost.ned@gmail.com>
2022-12-12 00:03:49 +0300

    add task45

--- Sem006/task45/Program.cs
+++ Sem006/task45/Program.cs
@@ -1,1 +1,12 @@
 ﻿// Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+int[] arr = {1,2,3,44,5,6};
+
+int[] arr2 = new int[arr.Length];
+
+for(int i = 0; i < arr.Length;i++)
+{
+    arr2[i] = arr[i];
+}
+Console.WriteLine($"[{string.Join(", ", arr)}]");
+Console.WriteLine($"[{string.Join(", ", arr2)}]");
