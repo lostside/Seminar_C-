b2bdfb67a7b4ef740c6ad5c02182bb882c5acf72
Denis <lost.ned@gmail.com>
2022-12-11 23:52:16 +0300

    added task44

--- Sem006/task44/Program.cs
+++ Sem006/task44/Program.cs
@@ -7,3 +7,18 @@
 // f(2) = 1
 // f(n) = f(n-1) + f(n -2)
 
+Console.WriteLine("Введите нужное количество первых цифр числа Фибоначчи");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] fibb = new int[n];
+
+fibb[0] = 0;
+fibb[1] = 1;
+
+for(int i = 2; i < n; i++)
+{
+    fibb[i] = fibb[i-1] + fibb[i-2];
+    
+}
+
+Console.WriteLine($"[{string.Join(" ", fibb)}]");
