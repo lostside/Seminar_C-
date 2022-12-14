938a40b0a84ab66482ee66db81631fe78d68f3cf
Denis <lost.ned@gmail.com>
2022-12-14 14:57:31 +0300

    added task49

---
+++ Sem007/task49/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, 
+// и замените эти элементы на их квадраты.​
+// Например, изначально массив
+// выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// ​
+// Новый массив будет выглядеть
+// вот так:
+// 1 4 7 2
+// 5 81 2 9
+// 8 4 2 4
+
+int[,] GetMatrix(int rowsCount, int columnsCount, int leftRange=0, int rightRange=9)
+{
+    int[,] matr = new int[rowsCount, columnsCount];
+    Random rand = new Random();
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = rand.Next(leftRange,rightRange + 1); // вместо 
+        }
+    }
+    return matr;
+}
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Find(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if((i+1) % 2 == 0 && (j+1) % 2 == 0)
+            {
+                matr[i, j] = matr[i, j] * matr[i, j];
+            } 
+        }
+    }
+}
+
+int rows = GetNumber("Введите число строк");
+int columns = GetNumber("Введите число столбцов");
+int[,] matrix = GetMatrix(rows, columns);
+PrintMatrix(matrix);
+Find(matrix);
+Console.WriteLine();
+PrintMatrix(matrix);
+
+void PrintMatrix(int [,] matr) // Вывод матрицы
+{
+    for (int i = 0; i < matr.GetLength(0); i++) // i < matrix.GetLenght(0). Этот цикл для строк
+    {
+        for( int j = 0; j < matr.GetLength(1); j++) // j < matrix.GetLength(1). Этот для столбцов
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
