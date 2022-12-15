9f7811d7ecb566dd90530c21d03991c2bd163022
Denis <lost.ned@gmail.com>
2022-12-15 14:08:00 +0300

    Added task47

--- Sem007/task47/Program.cs
+++ Sem007/task47/Program.cs
@@ -11,16 +11,16 @@ Random rand = new Random();
 //Console.WriteLine($"{rand.NextDouble() *(13.0 + 7.0) - 7.0:f2}");
 // rand.NextDouble() * (maxValue + minValue) + minValue
 
-int[,] GetMatrix(int rowsCount, int columnsCount, int leftRange=0, int rightRange=50)
+double[,] GetMatrix(int rowsCount, int columnsCount, int leftRange=0, int rightRange=50)
 {
-    int[,] matr = new int[rowsCount, columnsCount];
+    double[,] matr = new double[rowsCount, columnsCount];
     Random rand = new Random();
 
     for (int i = 0; i < matr.GetLength(0); i++)
     {
         for (int j = 0; j < matr.GetLength(1); j++)
         {
-            matr[i, j] = rand.Next(leftRange,rightRange + 1);
+            matr[i,j] = Convert.ToDouble(rand.NextDouble()*(10 + 9) - 7.0);
         }
     }
     return matr;
@@ -35,17 +35,18 @@ int GetNumber(string message)
 
 int rows = GetNumber("Введите число строк");
 int columns = GetNumber("Введите число столбцов");
-int[,] matrix = GetMatrix(rows, columns);
+double[,] matrix = GetMatrix(rows, columns);
 // Console.WriteLine();
 PrintMatrix(matrix);
 
-void PrintMatrix(int [,] matr) // Вывод матрицы
+void PrintMatrix(double [,] matr) // Вывод матрицы
 {
     for (int i = 0; i < matr.GetLength(0); i++) // i < matrix.GetLenght(0). Этот цикл для строк
     {
         for( int j = 0; j < matr.GetLength(1); j++) // j < matrix.GetLength(1). Этот для столбцов
         {
-            Console.Write($"{matr[i, j]} ");
+        
+            Console.Write($"{matr[i, j]:f2} ");
         }
     Console.WriteLine();
     }
