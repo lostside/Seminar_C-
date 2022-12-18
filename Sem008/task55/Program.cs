c71bfcefbe52c0a2d959626417dbf457d5bfd2c3
Denis <lost.ned@gmail.com>
2022-12-18 17:40:06 +0300

    added task55

---
+++ Sem008/task55/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+
+int[,] GetMatrix(int rowsCount, int columnsCount, int leftRange=0, int rightRange=9)
+{
+    int[,] matr = new int[rowsCount, columnsCount];
+    Random rand = new Random();
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = rand.Next(leftRange,rightRange + 1); // вместо 
+        }
+    }
+    return matr;
+}
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintMatrix(int [,] matr) // Вывод матрицы
+{
+    for (int i = 0; i < matr.GetLength(0); i++) // i < matrix.GetLenght(0). Этот цикл для строк
+    {
+        for( int j = 0; j < matr.GetLength(1); j++) // j < matrix.GetLength(1). Этот для столбцов
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+int rows = GetNumber("Введите число строк");
+int columns = GetNumber("Введите число столбцов");
+int[,] matrix = GetMatrix(rows, columns);
+PrintMatrix(matrix);
+
+void Recombinate(int[,] matr, int[,] newMatr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            newMatr[j, i] = matr[i,j];
+        }
+        
+    }
+}
+
+int[,] newMatr = new int[columns, rows];
+Recombinate(matrix,newMatr);
+Console.WriteLine();
+PrintMatrix(newMatr);
