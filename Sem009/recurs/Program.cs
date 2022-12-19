0e496a83cc4a4edcf97bb2c479ff44f891615fa9
Denis <lost.ned@gmail.com>
2022-12-19 20:29:34 +0300

    add recurs

--- Sem009/recurs/Program.cs
+++ Sem009/recurs/Program.cs
@@ -5,7 +5,7 @@
 // fact(5) = 120
 // fact(6) = 720
 uint Fact(uint number){ // uint = Положительное целое число
-    if(number == 0 || number == 1) // сначала делаем выход из рекурсии
+    if(number <= 1) // сначала делаем выход из рекурсии
     return 1;
     return number * Fact(number -1);
 }
