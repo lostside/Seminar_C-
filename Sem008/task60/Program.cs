3f0f1983bb02664729d4c718851ed1048c6a47b9
Denis <lost.ned@gmail.com>
2022-12-19 20:10:25 +0300

    added example task60

---
+++ Sem008/task60/Program.cs
@@ -0,0 +1,14 @@
+﻿int [,,] sheets = new int[2,2,2];
+int number = 10;
+
+for (int i = 0; i < sheets.GetLength(0); i++)
+{   
+    for (int j= 0; j < sheets.GetLength(1); j++)
+    {
+        for (int k = 0; k < sheets.GetLength(2); k++)
+        {
+            
+        }
+    }
+    
+}
