45ca4b146288ee77301aeeaa7ceae79416b343fa
Denis <lost.ned@gmail.com>
2022-12-21 00:52:27 +0300

    added task60

--- Sem008/task60/Program.cs
+++ Sem008/task60/Program.cs
@@ -1,4 +1,17 @@
-﻿int [,,] sheets = new int[2,2,2];
+﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, 
+// которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+// массив размером 2 x 2 x 2
+
+// 12(0,0,0) 22(0,0,1)
+
+// 45(1,0,0) 53(1,0,1)
+
+
+
+
+
+int [,,] sheets = new int[2,2,2];
 int number = 10;
 
 for (int i = 0; i < sheets.GetLength(0); i++)
@@ -7,9 +20,9 @@ for (int i = 0; i < sheets.GetLength(0); i++)
     {
         for (int k = 0; k < sheets.GetLength(2); k++)
         {
-            sheets[i,k,j] = number;
+            sheets[i,j,k] = number;
             number++;
-            //Console.WriteLine(i,k,j);
+            Console.WriteLine($"{sheets[i,j,k]} ({i}, {j}, {k})");
         }
     }
     
