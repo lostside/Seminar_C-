25a43f417026a25a1c9d28cc4fd97d1d6e0d3054
unknown <a@msk-sha-nb-0096.int.sblogistica.ru>
2022-12-22 17:42:09 +0300

    added task62

--- Sem008/task62/Program.cs
+++ Sem008/task62/Program.cs
@@ -6,3 +6,45 @@
 // 12 13 14 5
 // 11 16 15 6
 // 10 9 8 7
+
+int n = 4;
+int [,] spiral = new int[n,n];
+
+int i = 0;
+int j = 0;
+int a = 1;
+
+while (a <= spiral.GetLength(0) * spiral.GetLength(1))
+{
+    spiral[i,j] = a;
+    a++;
+    if (i <= j + 1 && i + j < spiral.GetLength(1) -1)
+    j++;
+    else if (i < j && i + j >= spiral.GetLength(0) -1)
+    i++;
+    else if (i >= j && i + j > spiral.GetLength(1) -1)
+    j--;
+    else
+     i--;
+
+}
+
+void FillArray(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0);i++)
+    {
+        for (int j = 0; j <array.GetLength(1);j++)
+        {
+            if (array[i,j] / 10 <=0)
+            {
+                Console.Write($" {array[i,j]} ");
+            }
+            else
+                Console.Write($"{array[i,j]} ");
+            
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArray(spiral);
