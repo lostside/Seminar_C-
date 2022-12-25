9ef103310ba33dd69cfdce1d00d7fc000141d029
Denis <lost.ned@gmail.com>
2022-12-25 22:05:04 +0300

    added not finished task41

--- Sem006/task41/Program.cs
+++ Sem006/task41/Program.cs
@@ -6,18 +6,21 @@
 // 1, -7, 567, 89, 223-> 3
 
 // break для остановки цикла
-int m=0;
 string s = "stop";
-string q = m.ToString();
-int count = 0;
-Console.WriteLine("Введите число");
-m = Convert.ToInt32(Console.ReadLine());
-if (q == s)
-//{   
-    //count = count + 1;
 
-    else
-        Console.WriteLine($"Количество положительных элементов {count}");
-        //break;
-           
-//}
+int[] collection = new int[10];
+int count = 0;
+
+for (int i = 0; i < collection.Length; i++)
+{
+    Console.WriteLine("Введите число ");
+    collection[i] = Convert.ToInt32(Console.ReadLine());
+    if (collection[i] > 0)
+    {
+        count++;
+    }
+    
+
+
+}
+Console.WriteLine("Чисел больше 0 = " + count);
