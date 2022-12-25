410dea2f34b4424ec77666daac5e0471540ba549
Denis <lost.ned@gmail.com>
2022-12-25 19:07:11 +0300

    added task68

--- Sem009/task68/Program.cs
+++ Sem009/task68/Program.cs
@@ -2,3 +2,21 @@
 // помощью рекурсии. Даны два неотрицательных числа m и n.
 
 // m = 2, n = 3 -> A(m,n) = 9
+
+Console.WriteLine("Введите число m");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int A(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0 && m > 0) return A(m - 1, 1);
+    else
+    {
+        return A(m - 1, A(m, n - 1));
+    }
+}
+
+int akkerman = A(m, n);
+Console.WriteLine(A(m, n));
