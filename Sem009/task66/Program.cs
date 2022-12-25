9a6b3fd8b2c725d7219e13aefa123e1a3b136e6d
Denis <lost.ned@gmail.com>
2022-12-25 18:52:39 +0300

    added task66(Почти приказ 66)))

--- Sem009/task66/Program.cs
+++ Sem009/task66/Program.cs
@@ -4,3 +4,29 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+Console.WriteLine("Введите число m");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void SumInterval(int m, int n, int sum)
+{
+    sum = sum + n;
+    if (n <= m)
+    {
+        Console.Write($" {sum} ");
+        return;
+    }
+    SumInterval(m, n - 1, sum);
+}
+
+int temp = m;
+
+if (m > n)
+{
+    m = n;
+    n = temp;
+}
+
+Console.Write($"{m}, {n} ->");
+SumInterval(m, n, temp = 0);
